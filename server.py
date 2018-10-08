4325cec1f3f11603b022190dcee01ab6140b9381
Shalom Steinbach <shalomss525@gmail.com>
2018-10-08 22:49:48 +0300

    add server.py and files for Heroku

---
+++ server.py
@@ -0,0 +1,8 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/<receiver_name>/<receiver_email>')
+def send_kim_mail(receiver_name, receiver_email):
+    os.system('python ./main.py %s %s Scotty %s smtp.gmail.com %s' % (receiver_name, receiver_email, os.environ['GMAIL_ADDR'], os.environ['GMAIL_PASS']))
+    return 'Sent Email'
