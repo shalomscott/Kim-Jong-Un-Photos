a6049001f371f26e982b2ffd67a9bae5d32f584d
Shalom Steinbach <shalomss525@gmail.com>
2018-10-09 19:27:43 +0300

    add more wordage

--- emailer.py
+++ emailer.py
@@ -16,14 +16,22 @@ subject = ['FYI',
            'Interesting',
            'See attached',
            'Photo',
-           'Classic Kim']
+           'Impressive',
+           'In case you missed it',
+           'Latest',
+           'Update',
+           'A little something',
+           'When you have a minute',
+           'New',
+           'Great stuff!']
 
 salutation = ['Mate',
               'Champ',
               'Fella',
               'Hombre',
               'Chief',
-              ]
+              'Friend',
+              'Comrade']
 
 first_line = ['Thought this might interest you. ',
               'Please see attached. ',
@@ -31,14 +39,42 @@ first_line = ['Thought this might interest you. ',
               'May be of interest. ',
               'Fascinating stuff. ',
               'Really thought-provoking. ',
-              'You ought to take a look at this. ']
+              'You ought to take a look at this. ',
+              'This really inspired me. ',
+              'Please consult: ',
+              'Thought I\'d pass it on to you. ',
+              'Really makes you appreciate. ']
+
+title = ['our Dear Leader',
+         'our Supreme Leader',
+         'our Respected Leader',
+         'The Marshall',
+         'our Beloved Leader',
+         'our Great Leader',
+         'our Wise Leader',
+         'our Brilliant Leader',
+         'The Great Leader Comrade',
+         'The Father of the People',
+         'Great General',
+         'The Great Sun of Life',
+         'our Peerless Leader',
+         'our Great Defender',
+         'His Excellency',
+         'The Ever-Victorious, Iron-Willed Commander']
 
 sign_off = ['Have a good one, ',
             'Best regards, ',
             'Regards, ',
             'See you anon, ',
             'We must do lunch some time soon, ',
-            'All the best, ']
+            'All the best, ',
+            'Cheers! ',
+            'Salute! ',
+            'To continued success, ',
+            'Talk soon, ',
+            'Hope this helped, ',
+            'Enjoy! ',
+            'Let\'s grab coffee soon, ']
 
 
 
@@ -51,7 +87,7 @@ class KimMail:
                  sender_email,
                  sender_server,
                  sender_password):
-        
+
         self.mail_photo = mail_photo
         self.sender_server = sender_server
         self.sender_password = sender_password
@@ -59,30 +95,31 @@ class KimMail:
         self.sender = sender
         self.sender_email = sender_email
         self.recipient_email = recipient_email
-        
+
         self.msg = email.mime.multipart.MIMEMultipart()
         self.msg['To'] = email.utils.formataddr((recipient, recipient_email))
         self.msg['From'] = email.utils.formataddr((sender, sender_email))
         self.msg['Subject'] = random.choice(subject)
-        
+
         msgText = email.mime.text.MIMEText(
-            "{},\n\n{}\n\nAs you'll see, it's a picture of Kim Jong Un {}.\n\n{}\n\n{}".format(
+            "{},\n\n{}\n\nAs you'll see, it's a picture of {} {}.\n\n{}\n\n{}".format(
                 random.choice(salutation),
                 random.choice(first_line),
+                random.choice(title),
                 mail_photo.title,
                 random.choice(sign_off),
             self.sender))
-        
+
         self.msg.attach(msgText)
-        
+
         with open(FILE, "rb") as f:
             pic = f.read()
-   
+
         att = email.mime.image.MIMEImage(pic)
-        self.msg.attach(att)        
-        
+        self.msg.attach(att)
+
         pass
-    
+
     def send(self):
         '''Send the KimMail'''
         print("[+]\tConnecting to {}...".format(self.sender_server))
@@ -97,7 +134,7 @@ class KimMail:
         print("[+]\tMessage sent. Quitting server")
         server.quit()
         self.update_cache()
-        
+
     def update_cache(self):
         if os.path.isfile(PATH+"/hist.log"):
             with open(PATH+"/hist.log", "rb") as f:
@@ -109,5 +146,5 @@ class KimMail:
             log={}
             log[self.recipient_email] = self.mail_photo.src
             with open(PATH+"/hist.log", "wb") as f:
-                pickle.dump(log, f)            
+                pickle.dump(log, f)
 
--- server.py
+++ server.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route('/<receiver_name>/<receiver_email>')
 def send_kim_mail(receiver_name, receiver_email):
-    os.system('python ./main.py %s %s Scotty %s smtp.gmail.com %s' % (receiver_name, receiver_email, os.environ['GMAIL_ADDR'], os.environ['GMAIL_PASS']))
+    os.system('python ./main.py %s %s Scott %s smtp.gmail.com %s' % (receiver_name, receiver_email, os.environ['GMAIL_ADDR'], os.environ['GMAIL_PASS']))
     return 'Sent Email'
